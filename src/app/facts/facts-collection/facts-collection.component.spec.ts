f0f424cb252a12db43a55fb2e3bd06200a46ebb0
JCBackendDev <jcbackenddev@gmail.com>
2022-10-31 10:17:40 +0100

    Added FactsCollection component

---
+++ src/app/facts/facts-collection/facts-collection.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FactsCollectionComponent } from './facts-collection.component';
+
+describe('FactsCollectionComponent', () => {
+  let component: FactsCollectionComponent;
+  let fixture: ComponentFixture<FactsCollectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FactsCollectionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FactsCollectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/facts/facts-collection/facts-collection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-facts-collection',
+  templateUrl: './facts-collection.component.html',
+  styleUrls: ['./facts-collection.component.css']
+})
+export class FactsCollectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
