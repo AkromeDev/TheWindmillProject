5ceeeb162ac5ce78c7cbae24becc452293a2e449
JCBackendDev <jcbackenddev@gmail.com>
2022-10-31 10:12:36 +0100

    Import GoalsDescription component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GoalsModule } from './goals/goals.module';
+import { PayPalComponent } from './pay-pal/pay-pal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PayPalComponent
   ],
   imports: [
     BrowserModule,
