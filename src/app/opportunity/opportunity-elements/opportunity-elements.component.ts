956ede9b31c3740d9b5a1037b4096e218ad47464
JCBackendDev <jcbackenddev@gmail.com>
2022-10-31 10:49:23 +0100

    Added opportunity element component

---
+++ src/app/opportunity/opportunity-elements/opportunity-elements.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-opportunity-elements',
+  templateUrl: './opportunity-elements.component.html',
+  styleUrls: ['./opportunity-elements.component.css']
+})
+export class OpportunityElementsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
