bde5b99a0f005159c012c9ce205f147da606481a
JCBackendDev <jcbackenddev@gmail.com>
2022-10-31 09:39:08 +0100

    Added GoalDescription component

--- src/app/goals/goals.module.ts
+++ src/app/goals/goals.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GoalsRoutingModule } from './goals-routing.module';
+import { GoalsDescriptionComponent } from './goals-description/goals-description.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GoalsDescriptionComponent
+  ],
   imports: [
     CommonModule,
     GoalsRoutingModule
