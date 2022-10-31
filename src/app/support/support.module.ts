b6fcd6973d82f799067a0a1073b094c840767a6e
JCBackendDev <jcbackenddev@gmail.com>
2022-10-31 10:14:44 +0100

    Moved paypal component into support module

--- src/app/support/support.module.ts
+++ src/app/support/support.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SupportRoutingModule } from './support-routing.module';
+import { PayPalComponent } from './pay-pal/pay-pal.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PayPalComponent
+  ],
   imports: [
     CommonModule,
     SupportRoutingModule
