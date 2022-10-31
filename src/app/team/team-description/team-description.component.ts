9d0c4b339e5c8fdd0cead64a8b14d581dde45772
JCBackendDev <jcbackenddev@gmail.com>
2022-10-31 10:53:07 +0100

    Added TeamDescription component

---
+++ src/app/team/team-description/team-description.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-team-description',
+  templateUrl: './team-description.component.html',
+  styleUrls: ['./team-description.component.css']
+})
+export class TeamDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
