3ba9f0cd446e8ef1640a4ce8e4b2ae720173cbcd
JCBackendDev <jcbackenddev@gmail.com>
2022-10-31 10:15:52 +0100

    Fix imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GoalsModule } from './goals/goals.module';
-import { PayPalComponent } from './pay-pal/pay-pal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PayPalComponent
   ],
   imports: [
     BrowserModule,
