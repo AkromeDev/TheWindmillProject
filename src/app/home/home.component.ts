2a2d65a8aecd357bde7cf18cbfc107f6bf315a29
JCBackendDev <jcbackenddev@gmail.com>
2022-11-03 10:31:51 +0100

    Implemented Divider Component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { DividerComponent } from '../standaloneComp/divider/divider.component';
 import { PlaceHolderComponent } from '../standaloneComp/place-holder/place-holder.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PlaceHolderComponent],
+  imports: [PlaceHolderComponent, DividerComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
