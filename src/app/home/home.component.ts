9f0a4e896bd8a9dfe128b7e19d214d049485135e
JCBackendDev <jcbackenddev@gmail.com>
2022-11-03 14:38:46 +0100

    App improvements

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { DividerComponent } from '../standaloneComp/divider/divider.component';
 import { PlaceHolderComponent } from '../standaloneComp/place-holder/place-holder.component';
+import { SegmentComponent } from '../standaloneComp/segment/segment.component';
+import { ExeriseComponent } from '../standaloneComp/exerise/exerise.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PlaceHolderComponent, DividerComponent],
+  imports: [PlaceHolderComponent, DividerComponent, SegmentComponent, ExeriseComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
