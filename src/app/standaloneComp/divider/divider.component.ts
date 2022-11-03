b722bb394bf29a7396effc02a8461973cc0c2e86
JCBackendDev <jcbackenddev@gmail.com>
2022-11-03 10:04:51 +0100

    Added Divider Component

---
+++ src/app/standaloneComp/divider/divider.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-divider',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './divider.component.html',
+  styleUrls: ['./divider.component.scss']
+})
+export class DividerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
