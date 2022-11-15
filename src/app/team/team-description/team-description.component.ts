782dda476cd8a605aea1633d09cde283338f48ab
JCBackendDev <jcbackenddev@gmail.com>
2022-11-15 09:06:25 +0100

    Implemented card components with all team members

--- src/app/team/team-description/team-description.component.ts
+++ src/app/team/team-description/team-description.component.ts
@@ -10,13 +10,36 @@ import { DividerComponent } from 'src/app/standaloneComp/divider/divider.compone
   styleUrls: ['./team-description.component.scss']
 })
 export class TeamDescriptionComponent implements OnInit {
-cards = [
-  { imageSrc: 'assets/images/OnOff/404OffTransparent.jpeg', buttonText: 'Text me'}
-];
-
+  cards = [
+    { 
+      imageSrc: 'assets/images/people/therese.jpeg', 
+      buttonText: 'Text me',
+      headerText: 'Thérèse Ringler, best of the best',
+      dateText: 'Since the 1st of January 2000, she is pretty young',
+      iconClass: 'venus mars',
+      iconText: 'She IS the man eater',
+    },
+    { 
+      imageSrc: 'assets/images/people/q.jpeg', 
+      buttonText: 'Squeeze me',
+      headerText: 'Q Quiblier Alex Antoine. Buveur de Picon Chouf',
+      dateText: 'Since Marseille existed, his soul was lighted on',
+      iconClass: 'bolt',
+      iconText: 'The Master of Apfel Apps',
+    },
+    { 
+      imageSrc: 'assets/images/people/j.jpeg', 
+      buttonText: 'Call me baby',
+      headerText: 'Mojo Chatelain, Aka Christ Juses',
+      dateText: 'Since the day he opened he flambed a fart',
+      iconClass: 'blind',
+      iconText: 'The Challenger',
+    },
+  ];
+  
   constructor() { }
-
+  
   ngOnInit(): void {
   }
-
+  
 }
