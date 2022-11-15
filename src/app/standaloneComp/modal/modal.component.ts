c0c37029dac02198139e31e119e5c59f6511c422
JCBackendDev <jcbackenddev@gmail.com>
2022-11-15 09:06:56 +0100

    created modal component

---
+++ src/app/standaloneComp/modal/modal.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
