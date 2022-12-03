ff418105cb646f7cb194fff5d0cd5c66cfe660e8
JCBackendDev <jcbackenddev@gmail.com>
2022-12-03 22:29:58 +0100

    Let's use the new Element

--- src/app/standaloneComp/one-page/one-page.component.ts
+++ src/app/standaloneComp/one-page/one-page.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit, HostListener } from '@angular/core';
 import { CardComponent } from '../card/card.component';
 import { ParallaxComponent } from '../parallax/parallax.component';
+import { TextBlockComponent } from '../text-block/text-block.component';
 import { TitleComponent } from '../title/title.component';
 
 @Component({
   selector: 'app-one-page',
   standalone: true,
-  imports: [CommonModule, CardComponent, ParallaxComponent, TitleComponent],
+  imports: [CommonModule, CardComponent, ParallaxComponent, TitleComponent, TextBlockComponent],
   templateUrl: './one-page.component.html',
   styleUrls: ['./one-page.component.scss']
 })
